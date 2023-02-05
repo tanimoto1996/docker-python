f0805c6170e36f6180c5505734720c4c90afc6c1
apple <apple@appurunoMacBook-Air.local>
2023-02-05 23:22:52 +0900

    google analytics初期画面遷移まで完了。

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 import time
 import chromedriver_binary
 chrome_options = webdriver.ChromeOptions()
@@ -10,16 +11,38 @@ driver = webdriver.Chrome('/usr/local/lib/python3.10/site-packages/chromedriver_
 driver.get('https://accounts.google.com/v3/signin/identifier?dsh=S419790722%3A1675580575528918&continue=https%3A%2F%2Fanalytics.google.com%2Fanalytics%2Fweb%2F%3Fauthuser%3D5%23%2Freport%2Fvisitors-overview%2Fa232476265w320180923p269594273%2F_u.date00%3D20230101%26_u.date01%3D20230204&followup=https%3A%2F%2Fanalytics.google.com%2Fanalytics%2Fweb%2F%3Fauthuser%3D5&passive=1209600&service=analytics&flowName=GlifWebSignIn&flowEntry=ServiceLogin&ifkv=AWnogHexwPhkGxFKp_scu-l0tLFlSbRo7M5oCrC1UGeMfeW4YXrdkhepwmxU62IHsoy31TWAn-QNdQ')
 print(driver.title)
 
+# メールアドレス入力画面
 mail = driver.find_element_by_name('identifier')
 mail.clear()
 mail.send_keys("k-tanimoto@mamiya-its.co.jp")
-button = driver.find_element_by_id('identifierNext')
-button.click()
-time.sleep(7)
-# search_box = driver.find_element_by_name("q")
-# search_box.send_keys('ChromeDriver')
-# search_box.submit()
-# print(driver.title)
+mail_button = driver.find_element_by_id('identifierNext')
+mail_button.click()
+time.sleep(2)
+
+# CloudGate UNO画面
+rawUsername = driver.find_element_by_id('rawUsername')
+rawUsername.clear()
+rawUsername.send_keys("k-tanimoto")
+
+realm = driver.find_element_by_id('realm')
+realm.click()
+
+realmOption = driver.find_element_by_id('realm-option-1')
+realmOption.click()
+
+realmButton = driver.find_element_by_id('username-realm-form')
+realmButton.submit()
+time.sleep(2)
+
+rawPassword = driver.find_element_by_id('password')
+rawPassword.clear()
+rawPassword.send_keys("N7vkGAV6")
+
+passwordButton = driver.find_element_by_xpath('//*[@id="password-form"]/button[1]')
+passwordButton.click()
+time.sleep(30)
+
+# ここからアナリティクス画面開始
 
 driver.save_screenshot('search_results2.png')
 driver.quit()
